5b836a81e10eec097f789839705f5b10fadb022d
younghelen342 <younghelen2000@yahoo.co.uk>
2021-03-13 19:21:46 +0000

    Sort age

--- LO.2&3.py
+++ LO.2&3.py
@@ -10,9 +10,25 @@ print(os.getcwd())
 data = pd.read_csv("Data Files\\healthcare-dataset-stroke-data.csv")
 data= data.drop(["id"], axis=1)
 print(data.isnull().sum())
-data = data.dropna(subset=['bmi'], axis=0)
+data= data.dropna(subset=['bmi'], axis=0)
 print(data.isnull().sum())
 print(data.shape)
+data["age"] = pd.to_numeric(data["age"], errors="coerce")
+data.sort_values("age")
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
