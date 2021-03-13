b802312d9ddf002d5aa7be893b04737403d3f9b9
younghelen342 <younghelen2000@yahoo.co.uk>
2021-03-13 15:33:41 +0000

    Added LO 4

---
+++ LO.4.py
@@ -0,0 +1,27 @@
+#   This function achieves the following learning outcomes :
+#       2. Importing data
+#       3. Analysing data
+
+import os
+
+import pandas as pd
+
+print(os.getcwd())
+data = pd.read_csv("Data Files\\healthcare-dataset-stroke-data.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
