e73a5a2a2ba9e9aa07926512f6035526d962a0d4
younghelen342 <younghelen2000@yahoo.co.uk>
2021-03-13 15:01:07 +0000

    Updated path of CSV file

--- New.py
+++ New.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 print(os.getcwd())
-data = pd.read_csv("healthcare-dataset-stroke-data.csv")
+data = pd.read_csv("Data Files\\healthcare-dataset-stroke-data.csv")
 print(data.head())
 print(data.tail())
 print(data.shape)
@@ -12,7 +12,7 @@ data.fillna
 data_filled = data.fillna(method = "ffill", axis = 0).fillna(0)
 print(data_filled.isnull().sum())
 data_final = data_filled.drop_duplicates(subset=["id"])
-print ("Heloo")
+
 
 
 
