faf9bc52dde340a9094a2f9c9a184f7d020eebf6
younghelen342 <younghelen2000@yahoo.co.uk>
2021-03-15 17:54:01 +0000

    Sort & Group

--- LO.2&3.py
+++ LO.2&3.py
@@ -3,9 +3,10 @@
 #       3. Analysing data
 
 import os
-
 import pandas as pd
-import numpy as py
+import numpy as np
+
+
 print(os.getcwd())
 data = pd.read_csv("Data Files\\healthcare-dataset-stroke-data.csv")
 data= data.drop(["id"], axis=1)
@@ -13,9 +14,13 @@ print(data.isnull().sum())
 data= data.dropna(subset=['bmi'], axis=0)
 print(data.isnull().sum())
 print(data.shape)
-data["age"] = pd.to_numeric(data["age"], errors="coerce")
-data.sort_values("age")
-print(data)
+data_sorted = data.sort_values("bmi", False)
+
+data_group = data.groupby("gender").count() 
+print(data_group)
+
+
+
 
 
 
