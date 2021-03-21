bb6402e9db9a6513b1e6ccbf32f2340da1238510
younghelen342 <younghelen2000@yahoo.co.uk>
2021-03-21 18:13:12 +0000

    Added Dictionary

--- LO.4.py
+++ LO.4.py
@@ -15,7 +15,7 @@ data = pd.read_csv("Data Files\\healthcare-dataset-stroke-data.csv")
 #Dropped ID as not required.
 data= data.drop(["id"], axis=1)
 
-print(data.isnull().sum())
+#print(data.isnull().sum())
 
 # Elimination of null values that I saw in BMI.
 data= data.dropna(subset=['bmi'], axis=0)
@@ -27,7 +27,20 @@ data.drop(lines_drop,inplace=True)
 # Using Numpy "Where" function to create a list showing low BMI
 bmi_low= np.where(data.bmi <18.5)
 bmi_low_list = data.iloc[bmi_low]
-print(bmi_low_list)
+#print(bmi_low_list)
+
+# Using dictionaries to convert the different categories in the dataset to numerical data.
+gender_dict = {"Male": 0, "Female": 1}
+relationship_dict = {"No" :0, "Yes":1}
+smoking_history_dict = {"Unknown" :0, "smokes":1, "never smoked":2, "formerly smoked": 3}
+residence_type_dict = {"Urban": 0, "Rural" :1}
+
+data["gender"] = data["gender"].map(gender_dict)
+data["ever_married"] = data["ever_married"].map(relationship_dict)
+data["smoking_status"] = data["smoking_status"].map(smoking_history_dict)
+data["Residence_type"] = data["Residence_type"].map(residence_type_dict)
+print(data)
+
 
 # #Converting dataframe columns "Age" and "Residence type to lists & using functions to manipulate the content
 # residence_list = data["Residence_type"].to_list()
